a3007ec973c5a75ca45e55aa65d1721dbdcabe14
Dominik Globisz <dominik.globiszz@gmail.com>
2026-05-03 17:26:20 +0200

    Updates

--- _next/static/olRgbd70uTIkip93nH-8X/_buildManifest.js
+++ _next/static/olRgbd70uTIkip93nH-8X/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/porsche-product-landpage//_next/:path+",
-        "destination": "/porsche-product-landpage/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
